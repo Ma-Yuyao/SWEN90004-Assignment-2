dd029204322f0898d79ce15dcef5c994044cc9b9
Ma-Yuyao <antyaoyao@gmail.com>
2021-05-15 12:08:20 +0800

    Updated daisy.py

--- daisy.py
+++ daisy.py
@@ -16,26 +16,26 @@ class Color(Enum):
     BLACK = 2
 
 class Daisy:
-    __color = None
-    __age = 0
-    __albedo = None
+    _color = None
+    _age = 0
+    _albedo = None
 
     def __init__(self, color):
-        self.__color = color
-        if (self.__color == Color.WHITE):
-            self.__albedo = WHITE_ALBEDO
-        elif (self.__color == Color.BLACK):
-            self.__albedo = BLACK_ALBEDO
+        self._color = color
+        if (self._color == Color.WHITE):
+            self._albedo = WHITE_ALBEDO
+        elif (self._color == Color.BLACK):
+            self._albedo = BLACK_ALBEDO
         '''
         根据代码，age 是一个 0 - MAX_AGE 的随机数
         '''
-        self.__age = random.randint(0, MAX_AGE)
+        self._age = random.randint(0, MAX_AGE)
 
     '''
     get_age 方法
     '''
     def get_age(self):
-        return self.__age
+        return self._age
 
 # Test
 class main():
